e68099899af8c09167317410db5e9368c5128ddc
StefanT <StefanT@DESKTOP-8F0JSC2>
2020-04-28 20:25:26 +0200

    Adding donate money page, chapter 4.7.4.

---
+++ src/main/java/de/dpunkt/myaktion/controller/DonateMoneyController.java
@@ -0,0 +1,67 @@
+package de.dpunkt.myaktion.controller;
+
+import java.io.Serializable;
+import java.util.ResourceBundle;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import de.dpunkt.myaktion.model.Donation;
+
+@SessionScoped
+@Named
+public class DonateMoneyController implements Serializable{
+
+	private String textColor = "000000";
+	private String bgColor = "ffffff";
+	private Long campaignId;
+	private Donation donation;
+	
+	public DonateMoneyController() {
+		this.setDonation(new Donation());
+	}
+
+	public String getTextColor() {
+		return textColor;
+	}
+
+	public void setTextColor(String textColor) {
+		this.textColor = textColor;
+	}
+
+	public String getBgColor() {
+		return bgColor;
+	}
+
+	public void setBgColor(String bgColor) {
+		this.bgColor = bgColor;
+	}
+
+	public Long getCampaignId() {
+		return campaignId;
+	}
+
+	public void setCampaignId(Long campaignId) {
+		this.campaignId = campaignId;
+	}
+
+	public Donation getDonation() {
+		return donation;
+	}
+
+	public void setDonation(Donation donation) {
+		this.donation = donation;
+	}
+	
+	public String doDonation() {
+		final FacesContext facesContext = FacesContext.getCurrentInstance();
+		final ResourceBundle resourceBundle = facesContext.getApplication().getResourceBundle(facesContext, "msg");
+		final String msg = resourceBundle.getString("donateMoney.thank_you");
+		facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+		this.donation = new Donation();
+		return Pages.DONATE_MONEY;
+	}
+
+}
