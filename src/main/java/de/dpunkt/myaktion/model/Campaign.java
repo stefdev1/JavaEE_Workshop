0cdb26f4c06d4462c942f6671359172fce5a43f0
StefanT <StefanT@DESKTOP-8F0JSC2>
2020-06-22 16:00:34 +0200

    Secure frontend by adding organizer and adding security domain, see chapter 8.2.1 - 8.2.4.

--- src/main/java/de/dpunkt/myaktion/model/Campaign.java
+++ src/main/java/de/dpunkt/myaktion/model/Campaign.java
@@ -10,6 +10,7 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -21,12 +22,14 @@ import javax.validation.constraints.Size;
 
 @Entity
 @NamedQueries({@NamedQuery(name = Campaign.findAll, query = "SELECT c FROM Campaign c ORDER BY c.name"),
+			   @NamedQuery(name = Campaign.findByOrganizer, query ="SELECT c FROM Campaign c WHERE c.organizer = :organizer ORDER BY c.name"),
 			   @NamedQuery(name = Campaign.getAmountDonatedSoFar, query = "SELECT SUM(d.amount) FROM Donation d WHERE d.campaign = :campaign")
 	})
 
 public class Campaign {
 	
 	public static final String findAll = "Campaign.findAll";
+	public static final String findByOrganizer = "Campaign.findByOrganizer";
 	public static final String getAmountDonatedSoFar = "Campaign.getAmountDonatedSoFar";
 	
 	@GeneratedValue
@@ -35,6 +38,9 @@ public class Campaign {
 
 	@OneToMany(mappedBy = "campaign", cascade = CascadeType.REMOVE)
 	private List<Donation> donations;
+	
+	@ManyToOne
+	private Organizer organizer;
 
 	@Embedded
 	@AttributeOverrides({@AttributeOverride(name = "name", column = @Column(name = "accountName"))})
@@ -113,6 +119,14 @@ public class Campaign {
 
 	public void setDonations(List<Donation> donations) {
 		this.donations = donations;
+	}
+
+	public Organizer getOrganizer() {
+		return organizer;
+	}
+
+	public void setOrganizer(Organizer organizer) {
+		this.organizer = organizer;
 	}
 	
 	
--- src/main/java/de/dpunkt/myaktion/services/CampaignServiceBean.java
+++ src/main/java/de/dpunkt/myaktion/services/CampaignServiceBean.java
@@ -2,12 +2,15 @@ package de.dpunkt.myaktion.services;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import de.dpunkt.myaktion.model.Campaign;
+import de.dpunkt.myaktion.model.Organizer;
 
 @Stateless
 public class CampaignServiceBean implements CampaignService{
@@ -15,9 +18,12 @@ public class CampaignServiceBean implements CampaignService{
 	@Inject
 	EntityManager entityManager;
 	
+	@Resource
+	private SessionContext sessionContext;
+	
 	@Override
 	public List<Campaign> getAllCampaigns() {
-		TypedQuery<Campaign> query = entityManager.createNamedQuery(Campaign.findAll, Campaign.class);
+		TypedQuery<Campaign> query = entityManager.createNamedQuery(Campaign.findByOrganizer, Campaign.class).setParameter("organizer", getLoggedinOrganizer());
 		List<Campaign> campaigns = query.getResultList();
 		campaigns.forEach(campaign -> campaign.setAmountDonatedSoFar(getAmountDonatedSoFar(campaign)));
 		
@@ -26,6 +32,8 @@ public class CampaignServiceBean implements CampaignService{
 
 	@Override
 	public void addCampaign(Campaign campaign) {
+		Organizer organizer = getLoggedinOrganizer();
+		campaign.setOrganizer(organizer);
 		entityManager.persist(campaign);	
 	}
 
@@ -51,6 +59,10 @@ public class CampaignServiceBean implements CampaignService{
 		return result;
 	}
 	
-	
+	private Organizer getLoggedinOrganizer() {
+		String organizerEmail = sessionContext.getCallerPrincipal().getName();
+		Organizer organizer = entityManager.createNamedQuery(Organizer.findByEmail, Organizer.class).setParameter("email", organizerEmail).getSingleResult();
+		return organizer;
+	}
 
 }
